12174979c010992d4a495e119a1a4e298ff54b2b
Radu-Linux <radupopa028@gmail.com>
2024-11-15 13:15:08 +0000

    Add function for IP validation using DNS server

--- validate_hosts.sh
+++ validate_hosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-while read -r line; do
-	echo "$line" | grep -Eo '([0-9]{1,3}\.{3}[0-9]{1,3}' > /dev/null
-	if [ $? -eq 0 ]; then
-		echo "Valid: $line"
-	else
-		echo "Invalid: $line"
-	fi
-done < /etc/hosts
+validate_ip() {
+	local host=$1
+	local ip=$2
+	local dns_server=$3
+	nslookup $host $dns_server | grep -q "$ip" && echo "Valid: $host - $ip" || echo "Invalid: $host - $ip"
+}
+
+validate_ip "localhost" "127.0.0.1" "8.8.8.8"
